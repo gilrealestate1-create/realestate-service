b7eef1930a4df0ceebaa7dde68341824d5836e48
mzhang61 <qq619992817@hotmail.com>
2025-09-13 21:05:52 -0500

    1

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+export default function Header() {
+    return (
+        <div className="site-header">
+            <div className="container left">
+                <Link to="/" className="brand">
+                    real-estate services
+                </Link>
+            </div>
+            <div className="nav">
+                <NavLink to="/" end className={({isActive}) => isActive ? 'active' : ''}>Home</NavLink>
+                <NavLink to="/about" className={({isActive}) => isActive ? 'active' : ''}>About Us</NavLink>
+                <NavLink to="/contact" className={({isActive}) => isActive ? 'active' : ''}>Contact Us</NavLink>
+            </div>
+        </div>
+    )
+}
